220975ac7f87a73d3483e5fe3c231d40ea467cb8
Paul Edmon <pedmon@DESKTOP-5GBIA4B>
2024-08-12 11:37:58 -0400

    Adding some more dummy stats to see how it looks.

--- files/login_slurm_stats.sh
+++ files/login_slurm_stats.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 
+echo ""
 echo "Greetings ${USER}! Here are some useful stats for you"
-echo "Current Fairshare Score: 0.5 | Usage Over Past Day: 500 CPU Hours 60 GPU Hours"
-echo "Jobs Run: 4000 | Job Efficiency: 50% CPU 50% Memory"
+echo "Current Fairshare Score: 0.5"
+echo "Usage Over Past Day: 500 CPU Hours 60 GPU Hours"
+echo "Jobs by State: 4000 Running 300 Completed 400 Canceled 500 Failed 3000 Out of Memory"
+echo "Average Number of Cores Used: 1 Allocated: 4 | Average Memory Used: 4GB Allocated: 8GB | Average Number of GPUs Used: 0"
+echo "Job Efficiency: 50% CPU 50% Memory"
