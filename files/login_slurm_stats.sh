a7c5924671038f80f2a334914cf95573fbea84b2
Paul Edmon <pedmon@DESKTOP-5GBIA4B>
2024-08-12 11:13:39 -0400

    Adding some dummy scripts for testing.

---
+++ files/login_slurm_stats.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+echo "Greetings ${USER}! Here are some useful stats for you"
+echo "Current Fairshare Score: 0.5 | Usage Over Past Day: 500 CPU Hours 60 GPU Hours"
+echo "Jobs Run: 4000 | Job Efficiency: 50% CPU 50% Memory"
--- manifests/login.pp
+++ manifests/login.pp
@@ -1,3 +1,10 @@
 # slurm_stats::login sets up a login node to print out slurm statistics on user login
 class slurm_stats::login {
+
+  file {'/etc/profile.d/zzz-slurm_stats.sh':
+    source => 'puppet:///module/slurm_stats/login_slurm_stats.sh',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
 }
