e31391a56d4f60914abea569862d7f122c1a8b71
Paul Edmon <pedmon@cfa.harvard.edu>
2024-08-13 14:19:04 -0400

    Updating name of script and adding cron.

--- manifests/data.pp
+++ manifests/data.pp
@@ -1,12 +1,21 @@
 # slurm_stats::data pulls the data
 class slurm_stats::data (
-  String $data_store = '/slurm/etc/slurm',
+  String  $data_store = '/slurm/etc/slurm',
+  Integer $cronhour   = '2',
+  Integer $cronmin    = '30',
 ){
 
-  file {'/usr/local/sbin/slurm_stats_fairshare':
-    content => template('slurm_stats/slurm_stats_fairshare.erb'),
+  file {'/usr/local/sbin/slurm_stats':
+    content => template('slurm_stats/slurm_stats.erb'),
     owner   => 'root',
     group   => 'root',
     mode    => '0755',
+  }
+
+  cron { 'slurm_stats':
+    command => '/usr/local/sbin/slurm_stats',
+    user    => 'root',
+    hour    => $cronhour,
+    minute  => $cronmin,
   }
 }
