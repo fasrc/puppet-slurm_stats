cde517b62e94b9ede8b56be77258650d5fde5052
Paul Edmon <pedmon@cfa.harvard.edu>
2024-08-13 11:31:38 -0400

    Moving to a template.

--- manifests/login.pp
+++ manifests/login.pp
@@ -3,6 +3,7 @@ class slurm_stats::login {
 
   file {'/etc/profile.d/zzz-slurm_stats.sh':
     source => 'puppet:///modules/slurm_stats/login_slurm_stats.sh',
+    content => template('slurm_stats/login_slurm_stats.sh.erb'),
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
