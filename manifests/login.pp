f38b0d4e236b70bb62250e4dd2ddd9f4ceeda8e4
Paul Edmon <pedmon@cfa.harvard.edu>
2024-08-13 14:37:14 -0400

    Adding cron on login node side.

--- manifests/login.pp
+++ manifests/login.pp
@@ -1,5 +1,9 @@
 # slurm_stats::login sets up a login node to print out slurm statistics on user login
-class slurm_stats::login {
+class slurm_stats::login (
+  String $data_store = '/slurm/etc/slurm',
+  Integer $cronhour   = 3,
+  Integer $cronmin    = 0,
+){
 
   file {'/etc/profile.d/zzz-slurm_stats.sh':
     source => 'puppet:///modules/slurm_stats/login_slurm_stats.sh',
@@ -7,5 +11,19 @@ class slurm_stats::login {
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
+  }
+
+  file {'/usr/local/share/slurm_stats':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  cron { 'slurm_stats':
+    command => '/usr/bin/cp -rf ${data_store}/slurm_stats* /usr/local/sbin/slurm_stats/.',
+    user    => 'root',
+    hour    => $cronhour,
+    minute  => $cronmin,
   }
 }
