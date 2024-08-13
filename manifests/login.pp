148493085719a4d90fb6e10ba653d1b5be0337c3
Paul Edmon <pedmon@cfa.harvard.edu>
2024-08-13 14:44:28 -0400

    Removing redundancy.

--- manifests/login.pp
+++ manifests/login.pp
@@ -1,12 +1,11 @@
 # slurm_stats::login sets up a login node to print out slurm statistics on user login
 class slurm_stats::login (
-  String $data_store = '/slurm/etc/slurm',
+  String  $data_store = '/slurm/etc/slurm',
   Integer $cronhour   = 3,
   Integer $cronmin    = 0,
 ){
 
   file {'/etc/profile.d/zzz-slurm_stats.sh':
-    source => 'puppet:///modules/slurm_stats/login_slurm_stats.sh',
     content => template('slurm_stats/login_slurm_stats.sh.erb'),
     owner  => 'root',
     group  => 'root',
