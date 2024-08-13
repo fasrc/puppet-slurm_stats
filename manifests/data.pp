9b5ce683fdafe600f9c3c30dd5b7f62c94e4ff01
Paul Edmon <pedmon@cfa.harvard.edu>
2024-08-13 10:15:15 -0400

    Fleshing out script.

--- manifests/data.pp
+++ manifests/data.pp
@@ -1,5 +1,7 @@
 # slurm_stats::data pulls the data
-class slurm_stats::data {
+class slurm_stats::data (
+  String $data_store = '/slurm/etc/slurm',
+){
 
   file {'/usr/local/sbin/slurm_stats_fairshare':
     content => template('slurm_stats/slurm_stats_fairshare.erb'),
