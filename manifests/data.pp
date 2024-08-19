9b612ea4a21b2eb5570ad124937f3451d9291c09
Paul Edmon <pedmon@DESKTOP-5GBIA4B>
2024-08-19 14:28:29 -0400

    Readding numpy.

--- manifests/data.pp
+++ manifests/data.pp
@@ -4,6 +4,8 @@ class slurm_stats::data (
   Integer $cronhour   = 2,
   Integer $cronmin    = 30,
 ){
+  ensure_packages(['python3-numpy'], {'ensure' => present})
+
   file {'/usr/local/sbin/slurm_stats':
     content => template('slurm_stats/slurm_stats.erb'),
     owner   => 'root',
